746f665c6d01a05771bc6efa3dcd1546048afa31
RichardStelzer <40047008+RichardStelzer@users.noreply.github.com>
2022-11-23 18:27:41 +0100

    Added new models "Ingredient", "Unit" and "IngredientQuantity". Migrated these models to the database.

--- recipe_manager_web_app/recipe/admin.py
+++ recipe_manager_web_app/recipe/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Recipe
+from .models import Unit, Ingredient, Recipe, IngredientQuantity
 
-
+admin.site.register(Unit)
+admin.site.register(Ingredient)
 admin.site.register(Recipe)
+admin.site.register(IngredientQuantity)
--- recipe_manager_web_app/recipe/models.py
+++ recipe_manager_web_app/recipe/models.py
@@ -1,16 +1,43 @@
 from django.db import models
 
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Recipe(models.Model):
     author = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     brief_description = models.TextField()
     full_description = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient, through="IngredientQuantity")
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ("-created_at",)
 
-    # def __str__(self):
-    #     return self.title
+    def __str__(self):
+        return self.title
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=255, default=None, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class IngredientQuantity(models.Model):  # Join the db tables
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+
+    quantity = models.IntegerField()
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.recipe.title + " - " + self.ingredient.name
+
