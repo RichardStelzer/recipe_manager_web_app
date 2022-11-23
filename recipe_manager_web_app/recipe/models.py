b26aa73a88cc12b4c5a7c67ddb0ba1d75b6f813c
RichardStelzer <40047008+RichardStelzer@users.noreply.github.com>
2022-11-23 03:57:35 +0100

    Improved the visuals with the help of the bulma css framework and a great drawing.

--- recipe_manager_web_app/recipe/models.py
+++ recipe_manager_web_app/recipe/models.py
@@ -5,8 +5,8 @@ class Recipe(models.Model):
     author = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField()
-    instruction = models.TextField()
+    brief_description = models.TextField()
+    full_description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
